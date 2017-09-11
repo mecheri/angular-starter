49255b03f1f408bde8aeaeb4e2baf8a43b1b54f0
MECHERI Mehdi <m.mecheri@Viveris.lan>
2017-09-11 14:15:19 +0200

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { routing } from "./app.routing";
 
 // Components
 import { AppComponent } from "./app.component";
+
 import { IndexComponent } from "./components/index/index.component";
 import { LoginComponent } from "./components/login/login.component";
 import { HomeComponent } from "./components/home/home.component";
